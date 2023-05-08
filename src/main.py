65beb646cea86393b0543e3b9dd0695223dab23a
Georgios Giasemidis <g.giasemidis@gmail.com>
2023-05-08 02:24:46 +0100

    max tokens set to 256

--- src/main.py
+++ src/main.py
@@ -14,15 +14,15 @@ def main():
     st.write("**Example:** *'Who won the NBA finals in 2011 and who won the finals MVP?'*")
     st.write("I don't answer questions like *'Who was US president in 2010?'*")
 
-    input_txt = st.text_area("Ask me your question. Answers will be limited to 30 tokens")
+    input_txt = st.text_area("Ask me your question. Answers will be limited to 256 tokens")
 
-    prompt = "You are a basketball specialist. If a question is not related to basketball answer 'This is a non-basketball question'. Limit your answer to 30 tokens if possible"
+    prompt = "You are a basketball specialist. If a question is not related to basketball answer 'This is a non-basketball question'. Limit your answer to 256 tokens if possible"
 
     if input_txt != "":
         # st.write("You entered:", input_txt)
         prompt += input_txt
 
-        response = openai.Completion.create(model="text-davinci-003", prompt=prompt, max_tokens=30)
+        response = openai.Completion.create(model="text-davinci-003", prompt=prompt, max_tokens=256)
         # Print answer to the screen
         st.write(response["choices"][0]["text"])
 
